4302898b29b84a1f7eefa7aa79ef1201e849a001
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2024-03-19 11:55:17 +0100

    ajout du sin

--- src/script.js
+++ src/script.js
@@ -98,7 +98,7 @@ material.onBeforeCompile = (shader) => {
     "#include <beginnormal_vertex>",
     `
     #include <beginnormal_vertex>
-    float angle = (position.y + uTime) * 0.5;
+    float angle = (sin(position.y + uTime)) * 0.4;
     mat2 rotateMatrix = get2dRotateMatrix(angle);
 
     objectNormal.xz = rotateMatrix * objectNormal.xz;
@@ -134,7 +134,8 @@ depthMaterial.onBeforeCompile = (shader) => {
     "#include <begin_vertex>",
     `
       #include <begin_vertex>
-      float angle = (position.y + uTime) * 0.5;
+
+      float angle = (sin(position.y + uTime)) * 0.4;
       mat2 rotateMatrix = get2dRotateMatrix(angle);
   
       transformed.xz = rotateMatrix * transformed.xz;
