96d73b86c20bb8653474b26eed7358653e78ba15
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2024-03-19 11:48:41 +0100

    fix normal

--- src/script.js
+++ src/script.js
@@ -93,13 +93,23 @@ material.onBeforeCompile = (shader) => {
 
     `
   );
+
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <beginnormal_vertex>",
+    `
+    #include <beginnormal_vertex>
+    float angle = (position.y + uTime) * 0.5;
+    mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+    objectNormal.xz = rotateMatrix * objectNormal.xz;
+    `
+  );
+
   shader.vertexShader = shader.vertexShader.replace(
     "#include <begin_vertex>",
     `
     #include <begin_vertex>
-    float angle = (position.y + uTime) * 0.9;
-    mat2 rotateMatrix = get2dRotateMatrix(angle);
-
+  
     transformed.xz = rotateMatrix * transformed.xz;
     `
   );
@@ -118,14 +128,13 @@ depthMaterial.onBeforeCompile = (shader) => {
       {
           return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
       }
-  
       `
   );
   shader.vertexShader = shader.vertexShader.replace(
     "#include <begin_vertex>",
     `
       #include <begin_vertex>
-      float angle = (position.y + uTime) * 0.9;
+      float angle = (position.y + uTime) * 0.5;
       mat2 rotateMatrix = get2dRotateMatrix(angle);
   
       transformed.xz = rotateMatrix * transformed.xz;
